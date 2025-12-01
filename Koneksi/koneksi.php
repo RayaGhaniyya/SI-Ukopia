765786826fb855d6b1a331bec1b74fb828be6c2c
ramaaarv <ramaalisyahbana@gmail.com>
2025-12-02 00:45:18 +0700

    loyalty

--- Koneksi/koneksi.php
+++ Koneksi/koneksi.php
@@ -7,6 +7,5 @@ $database   = "db_ukopia";
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+    
 }
-?>
