9938253ce57ae17774578d81f51dc5dda7c72063
ray4c <122428288+RayaGhaniyya@users.noreply.github.com>
2025-12-03 03:54:09 +0700

    responsive

--- BackOffice/Component/head.php
+++ BackOffice/Component/head.php
@@ -13,6 +13,13 @@
     <link rel="stylesheet" href="/SI-Ukopia/BackOffice/assets/css/transaksi.css">
     <link rel="stylesheet" href="/SI-Ukopia/BackOffice/assets/css/dashboard.css">
 
+
+    <link rel="stylesheet" href="/SI-Ukopia/BackOffice/assets/css/responsive/dashboard-responsive.css">
+    <link rel="stylesheet" href="/SI-Ukopia/BackOffice/assets/css/responsive/products-responsive.css">
+    <link rel="stylesheet" href="/SI-Ukopia/BackOffice/assets/css/responsive/sidebar-responsive.css">
+    <link rel="stylesheet" href="/SI-Ukopia/BackOffice/assets/css/responsive/misc-responsive.css">
+    <link rel="stylesheet" href="/SI-Ukopia/BackOffice/assets/css/responsive/global-responsive.css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
